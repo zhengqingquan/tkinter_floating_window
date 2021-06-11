04d7b5fd502191e9c234d8feee3b877878d37d3d
zhengqingquan <55186872+zhengqingquan@users.noreply.github.com>
2021-06-11 09:25:49 +0800

    commit
    Fixed bug: hover Windows will not be created if you have created a virtual desktop before.

--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 A Tkinter window that will not be killed by the display desktop
 
 这是一个tkinter的悬浮窗口，并且不会被win10的显示桌面给隐藏掉。甚至还可以自由拖动窗口位置。
-但缺点是无法将窗口置为顶层（用于显示在最前面）。或许在之后可以找到方法修改。
+缺点是无法将窗口置为顶层（用于显示在最前面）。或许在之后可以找到方法修改。
 
+# demo pictures
 ![image](https://github.com/zhengqingquan/gallery/blob/main/tkinter_floating_window/Video_2021-06-10_171212.gif?raw=true)   
 
-# library
-这里使用了win32gui的库
+# library & Train of thought
+这里使用了win32gui的库。
+通过将tkinter的父窗口设置为桌面，来达到预定效果。
+
+# change
+1.1.0 修改错误：当你曾创建过虚拟桌面时，将无法创建悬浮窗口。
+1.0.0 初版：可以在window10的桌面创建悬浮窗口，并且不受“显示桌面”功能的影响。
--- scr/floatingwindow.py
+++ scr/floatingwindow.py
@@ -1,5 +1,6 @@
 """
-tkinter
+FloatingWindow
+windows10的悬浮窗口。可以不受windows10任务栏的“显示桌面”功能影响。
 """
 import tkinter as tk
 import win32gui
@@ -7,7 +8,8 @@ import win32gui
 
 class FloatingWindow(tk.Tk):
     def __init__(self):
-        super().__init__()
+        super().__init__()  # 先进行tk.Tk的初始化
+        self.SysListView32_handle = 0  # 保存SysListView32窗口的句柄
         self.overrideredirect(True)  # 建立一个没有任何按钮，无法关闭，最大化，最小化的窗口。该方法必须放在init_parent_window()之前，否则会被显示桌面隐藏。
         self.wm_attributes("-toolwindow", True)  # 置为工具窗口（右上角只有关闭按钮）。该方法必须放在init_parent_window()之前，否则会被显示桌面隐藏。
         self.update()  # 初始化后刷新窗口数据，否则只有tkinter的初始值。这样会导致找不到顶级窗口句柄。
@@ -19,27 +21,37 @@ class FloatingWindow(tk.Tk):
         # self.self_handle = self.winfo_id() # 十进制的句柄（类名是TkChild）
 
     def init_parent_window(self):
-        # 获得桌面的句柄
-        Progman_handle = win32gui.FindWindow("Progman", None)
-        # GW_HWNDFIRST = 0  # 同级别第一个窗口
-        # GW_HWNDLAST = 1  # 同级别最后一个窗口
-        # GW_HWNDNEXT = 2  # 同级别下一个窗口
-        # GW_HWNDPREV = 3  # 同级别上一个窗口
-        # GW_OWNER = 4  # 属主窗口
-        GW_CHILD = 5  # 子窗口
-        # GW_ENABLEDPOPUP = 6  # enabled popup window有效的弹出窗口
-        SHELLDLL_DefView_handle = win32gui.GetWindow(Progman_handle, 5)
-        SysListView32_handle = win32gui.GetWindow(SHELLDLL_DefView_handle, GW_CHILD)
-        print(f"桌面的句柄为：{SysListView32_handle}")
+        # 使用枚举遍历获取SysListView32的句柄。
+        win32gui.EnumWindows(self.find_SysListView32, 0)
+        print(f"SysListView32_handle的句柄为：{self.SysListView32_handle}")
 
-        # 获得顶级窗口的句柄
+        # 获得自身顶级窗口的句柄
         tk_top = win32gui.FindWindow("TkTopLevel", self.title())
+        print(self.title())
         print(f"TkTopLevel的句柄为：{tk_top}")
 
         # 设置父窗口为桌面
-        win32gui.SetParent(tk_top, SysListView32_handle)
+        win32gui.SetParent(tk_top, self.SysListView32_handle)
+
+    def find_SysListView32(self, hwnd, mouse):
+        """
+        用来查找桌面SysListView32的回调函数。
+        :param hwnd:窗口的句柄
+        :param mouse:回调函数的参数，该值为0。
+        :return:None
+        """
+        if win32gui.IsWindow(hwnd) and win32gui.IsWindowEnabled(hwnd) and win32gui.IsWindowVisible(
+                hwnd) and win32gui.GetWindow(hwnd, 5):  # 窗口存在，且窗口可用，且窗口看得见，且窗口有子窗口。
+            if win32gui.GetClassName(win32gui.GetWindow(hwnd, 5)) == 'SHELLDLL_DefView':
+                # print(win32gui.GetClassName(win32gui.GetWindow(win32gui.GetWindow(hwnd, 5), 5)))
+                # print(win32gui.GetWindow(win32gui.GetWindow(hwnd, 5), 5))
+                self.SysListView32_handle = win32gui.GetWindow(win32gui.GetWindow(hwnd, 5), 5)
 
     def init_style(self):
+        """
+        用来初始化窗口的风格（样式）
+        :return:None
+        """
         width = None
         height = None
         self.width, self.height = width, height
@@ -48,7 +60,11 @@ class FloatingWindow(tk.Tk):
         self.wm_attributes("-alpha", 0.4)  # 透明度。更改透明度必须放init_parent_window()后面，否则窗口会消失。
 
     def init_event(self):
-        self.bind('<B1-Motion>', self._on_move)  # 拖动左键
+        """
+        用来添加窗口的事件
+        :return:None
+        """
+        self.bind('<B1-Motion>', self._on_move)  # 左键拖动
         self.bind('<ButtonPress-1>', self._on_tap)  # 单击左键
 
     # def set_display_postion(self, offset_x, offset_y):
@@ -59,6 +75,11 @@ class FloatingWindow(tk.Tk):
     #     self.geometry("%sx%s" % (w, h))
 
     def _on_move(self, event):
+        """
+        左键拖动事件
+        :param event:事件参数
+        :return:
+        """
         offset_x = event.x_root - self.root_x
         offset_y = event.y_root - self.root_y
 
@@ -70,8 +91,14 @@ class FloatingWindow(tk.Tk):
         self.geometry(geo_str)
 
     def _on_tap(self, event):
+        """
+        单击左键事件
+        :param event: 事件参数
+        :return:
+        """
         self.root_x, self.root_y = event.x_root, event.y_root
         self.abs_x, self.abs_y = self.winfo_x(), self.winfo_y()
+
 
 if __name__ == '__main__':
     fw = FloatingWindow()
